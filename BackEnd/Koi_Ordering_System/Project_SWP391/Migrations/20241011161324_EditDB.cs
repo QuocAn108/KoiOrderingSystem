24cb97dd9c56cbf6ad39383513d193733c03ada9
Ryan <anhoang800140@gmail.com>
2024-10-21 23:50:14 +0700

    Push all

---
+++ BackEnd/Koi_Ordering_System/Project_SWP391/Migrations/20241011161324_EditDB.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Project_SWP391.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Bills_UserId",
+                table: "Bills");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UpdateDate",
+                table: "Kois",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bills_UserId",
+                table: "Bills",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Bills_UserId",
+                table: "Bills");
+
+          
+            migrationBuilder.AlterColumn<string>(
+                name: "UpdateDate",
+                table: "Kois",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bills_UserId",
+                table: "Bills",
+                column: "UserId",
+                unique: true);
+        }
+    }
+}
---
+++ BackEnd/Koi_Ordering_System/Project_SWP391/Migrations/20241014093313_ChangeDB.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Project_SWP391.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Quotations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Quotations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Quotations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Quotations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+           
+        }
+
+    }
+}
